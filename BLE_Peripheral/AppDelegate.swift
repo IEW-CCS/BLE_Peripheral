fbfc7ab7224333b03c9ac742386cb30c0bebcbd7
James <robohood83@gmail.com>
2019-08-20 09:04:44 +0800

    Initial Commit

---
+++ BLE_Peripheral/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BLE_Peripheral
+//
+//  Created by Lo Fang Chou on 2019/8/20.
+//  Copyright © 2019 JStudio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BLE_Peripheral/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BLE_Peripheral
+//
+//  Created by Lo Fang Chou on 2019/8/20.
+//  Copyright © 2019 JStudio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
